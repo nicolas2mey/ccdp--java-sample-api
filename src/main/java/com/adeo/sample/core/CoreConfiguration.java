d74c3b047d4dd4b3f729f43b3f299457092e2d8f
Jérémy Florte <jeremy.florte@leroymerlin.fr>
2021-10-14 11:47:15 +0200

    Feat/Upgrade github runners, Spring Boot version and add threads display at startup.

--- src/main/java/com/adeo/sample/core/CoreConfiguration.java
+++ src/main/java/com/adeo/sample/core/CoreConfiguration.java
@@ -1,6 +1,8 @@
 package com.adeo.sample.core;
 
+import com.adeo.sample.core.services.ThreadService;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,4 +16,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @ComponentScan(basePackageClasses = RestControllerAdvice.class)
 @NoArgsConstructor
 public class CoreConfiguration {
+
+    /**
+     * Creates and exposes {@link ThreadService} instance.
+     *
+     * @return a {@link ThreadService} instance.
+     */
+    @Bean
+    public ThreadService getThreadService() {
+        return new ThreadService();
+    }
 }
---
+++ src/main/java/com/adeo/sample/core/services/ThreadService.java
@@ -0,0 +1,58 @@
+package com.adeo.sample.core.services;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Service used to get and display thread information.
+ *
+ * @author ccdccloudops@adeo.com
+ */
+@Slf4j
+public class ThreadService {
+
+    private final ThreadInfo threadInfo;
+
+    /**
+     * Creates a new {@link ThreadService} instance.
+     */
+    public ThreadService() {
+        this.threadInfo = new ThreadInfo();
+    }
+
+    /**
+     * Runs service.
+     */
+    @PostConstruct
+    public void run() {
+
+        Thread.getAllStackTraces()
+              .keySet()
+              .forEach(this.threadInfo::add);
+
+        log.info(this.threadInfo.toString());
+    }
+
+    @Value
+    private static class ThreadInfo {
+
+        Map<String, List<String>> threads;
+
+        private ThreadInfo() {
+            this.threads = new ConcurrentHashMap<>();
+        }
+
+        private void add(final Thread thread) {
+            final var groupName = thread.getThreadGroup().getName();
+            
+            this.threads.computeIfAbsent(groupName, key -> new ArrayList<>());
+            this.threads.get(groupName).add(thread.getName());
+        }
+    }
+}
